3860dfe7a7e298f9e152b44e0436f40cb7b638ba
Owen Thomas <owensamthomas@gmail.com>
2025-03-24 17:14:31 -0400

    Update future_figures.m
    updated documentation

--- model/future_figures.m
+++ model/future_figures.m
@@ -1,3 +1,16 @@
+%{
+This code is used to create regime diagrams showing community behaviors and
+nourishment volumes based on .mat data files. These diagrams are based on
+data outputs from future_analysis.m.
+
+To use this script, select the mat file you would like to load from the
+vector "mat_files" in the two sections below. You can also add your mat
+file name to that variable if you have created your own data file.
+
+This code produces two extra diagrams not included in the associated
+manuscript that may be of value to your understanding of the data.
+%}
+
 %% Data Files
 % Vector of data files to be studied
 mat_files=["future_analysis_cs_ba_v1.mat",...
